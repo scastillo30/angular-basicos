bcacbb77c1542f21991dee4d31e1564d61d0bef2
Sebastian Castillo <scastillo27@gmail.com>
2021-03-03 16:25:15 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
